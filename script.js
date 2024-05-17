7191c1852ba4b119a5c8b3dcbbdc19729322bc52
TeeDoubleYooh <taylorexchange004@gmail.com>
2024-05-17 16:20:11 +0100

    added random enemy movement, enemy collision, score checker, lives function, restart function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+let restart = document.querySelector('.restart')
+restart.style.display = 'none';
 let upPressed = false;
 let downPressed = false;
 let leftPressed = false;
@@ -9,7 +12,7 @@ const main = document.querySelector('main');
 let maze = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 2, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 7, 0, 0, 0, 0, 0, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
@@ -18,6 +21,8 @@ let maze = [
     [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
+
+
 
 //Random Enemy Testing
 // function randomEnemy() {
@@ -48,6 +53,152 @@ randomEnemy();
 
 
 
+// enemies.forEach(enemy => {
+//     let enemyPos = enemy.getBoundingClientRect();
+//     let enemyTop = parseInt(enemy.style.top)  0; //PARSE INT SO ITS NOT RETURNING A STRING!!!!
+//     let enemyLeft = parseInt(enemy.style.left)  0;
+//     let direction = enemy.direction || randomNumber();
+
+// if (randomNumber == 1) {
+//     let position = enemy.getBoundingClientRect()
+//     let newBottom = position.bottom + 1;
+
+//     let btmL = document.elementFromPoint(position.left, newBottom)
+//     let btmR = document.elementFromPoint(position.right, newBottom)
+//     if (btmL.classList.contains('wall') == false && btmR.classList.contains('wall') == false) {
+//         enemyTop++;
+//         enemy.style.top = enemyTop + 'px';
+//     }
+// }
+
+
+
+
+// setInterval(function () {
+//     let enemies = document.querySelectorAll('.enemy')
+//     for (const enemy of enemies){
+//         const enemyPosition = enemy.getBoundingClientRect();
+//     }
+//         if (randomNumber == 1) {
+//             let position = enemy.getBoundingClientRect()
+//             let enemyBottom = position.bottom + 1;
+
+//             let btmL = document.elementFromPoint(position.left, enemyBottom)
+//             let btmR = document.elementFromPoint(position.right, enemyBottom)
+//             if (btmL.classList.contains('wall') == false && btmR.classList.contains('wall') == false) {
+//                 enemyTop++;
+//                 enemy.style.top = enemyTop + 'px';
+//             }
+//         }
+//         else if (randomNumber == 2) {
+//             let position = enemy.getBoundingClientRect()
+//             let enemyTop = position.top - 1;
+
+//             let topL = document.elementFromPoint(position.left, enemyTop)
+//             let topR = document.elementFromPoint(position.right, enemyTop)
+//             if (topL.classList.contains('wall') == false && topR.classList.contains('wall') == false) {
+//                 enemyTop--;
+//                 enemy.style.top = enemyTop + 'px';
+//             }
+//         }
+//         else if (randomNumber == 3) {
+//             let position = enemy.getBoundingClientRect()
+//             let enemyLeft = position.left - 1;
+
+//             let topL = document.elementFromPoint(enemyLeft, position.top)
+//             let btmL = document.elementFromPoint(enemyLeft, position.bottom)
+//             if (topL.classList.contains('wall') == false && btmL.classList.contains('wall') == false) {
+//                 enemyLeft--;
+//                 enemy.style.left = enemyLeft + 'px';
+//             }
+//         }
+//         else if (rightPressed) {
+//             let position = enemy.getBoundingClientRect()
+//             let enemyRight = position.right + 1;
+
+//             let topR = document.elementFromPoint(enemyRight, position.top)
+//             let btmR = document.elementFromPoint(enemyRight, position.bottom)
+//             if (topR.classList.contains('wall') == false && btmR.classList.contains('wall') == false) {
+//                 enemyLeft++;
+//                 enemy.style.left = enemyLeft + 'px';
+//             }
+//         }
+// }, 100);
+let enemyTop = 0;
+let enemyLeft = 0;
+
+function randomNumber() {
+    return Math.floor(Math.random() * 4) + 1;
+}
+
+let enemies = document.querySelectorAll('.enemy');
+let holdEnemy = true;
+setInterval(function moveEnemy() {
+    if (holdEnemy) return;
+
+    enemies = document.querySelectorAll('.enemy');
+
+    enemies.forEach(enemy => {
+        let enemyPos = enemy.getBoundingClientRect();
+        let direction = enemy.direction || randomNumber();
+        let enemyTop = parseInt(enemy.style.top) || 0;
+        let enemyLeft = parseInt(enemy.style.left) || 0;
+
+        switch (direction) {
+            case 1: // MOVE DOWN
+                newBottom = enemyPos.bottom + 1;
+                btmL = document.elementFromPoint(enemyPos.left, newBottom);
+                btmR = document.elementFromPoint(enemyPos.right, newBottom);
+
+                if (!btmL.classList.contains('wall') && !btmR.classList.contains('wall')) {
+                    enemyTop++;
+                } else {
+                    direction = randomNumber();
+                }
+                break;
+
+            case 2: // MOVE UP
+                newTop = enemyPos.top - 1;
+                topL = document.elementFromPoint(enemyPos.left, newTop);
+                topR = document.elementFromPoint(enemyPos.right, newTop);
+
+                if (!topL.classList.contains('wall') && !topR.classList.contains('wall')) {
+                    enemyTop--;
+                } else {
+                    direction = randomNumber();
+                }
+                break;
+
+            case 3: // MOVE LEFT
+                newLeft = enemyPos.left - 1;
+                leftTop = document.elementFromPoint(newLeft, enemyPos.top);
+                leftBottom = document.elementFromPoint(newLeft, enemyPos.bottom);
+
+                if (!leftTop.classList.contains('wall') && !leftBottom.classList.contains('wall')) {
+                    enemyLeft--;
+                } else {
+                    direction = randomNumber();
+                }
+                break;
+
+            case 4: // MOVE RIGHT
+                newRight = enemyPos.right + 1;
+                rightTop = document.elementFromPoint(newRight, enemyPos.top);
+                rightBottom = document.elementFromPoint(newRight, enemyPos.bottom);
+
+                if (!rightTop.classList.contains('wall') && !rightBottom.classList.contains('wall')) {
+                    enemyLeft++;
+                } else {
+                    direction = randomNumber();
+                }
+                break;
+        }
+
+        enemy.style.top = enemyTop + 'px';
+        enemy.style.left = enemyLeft + 'px';
+        enemy.direction = direction;
+    });
+}, 10);
 
 
 //Populates the maze in the HTML
@@ -162,9 +313,13 @@ setInterval(function () {
             player.style.left = playerLeft + 'px';
         }
         playerMouth.classList = 'right';
-    }
+
+    } 
+    enemyCollision();
 }, 10);
 
+
+let score = 0;
 function pointCheck() {
     const position = player.getBoundingClientRect();
     const points = document.querySelectorAll('.point');
@@ -178,7 +333,13 @@ function pointCheck() {
             position.top < pos.bottom
         ) {
             points[i].classList.remove('point');
+            score++;
+            document.querySelector('.score p').textContent = score;
         }
+    }
+    if (points.length <= 0) {
+
+        endGame();
     }
 }
 
@@ -191,25 +352,78 @@ function setColor() {
     player.style.backgroundColor = this.id;
 }
 // end
+let lives = 3
+function livesFunc() {
+    let lives = document.querySelectorAll('.lives ul li');
+    lives[0].parentNode.removeChild(lives[0]);
+}
+let hit = false;
+function enemyCollision() {
+    if(hit) return;
+    let position = player.getBoundingClientRect();
+    let enemies = document.querySelectorAll('.enemy');
+    for (const enemy of enemies) {
+        const posEnemy = enemy.getBoundingClientRect();
 
+        if (
+            position.right > posEnemy.left &&
+            position.left < posEnemy.right &&
+            position.bottom > posEnemy.top &&
+            position.top < posEnemy.bottom
+        ) {
+            player.classList.add('hit');
+            hit = true;
+            lives--;
+            livesFunc();
 
-
-
-
-
-const start = document.querySelector('#startButton');
-function startGame() {
-
-    start.style.display = 'none';
-
-    document.addEventListener('keydown', keyDown);
-    document.addEventListener('keyup', keyUp);
-
-   
-
-
+            setTimeout(() =>{
+                player.classList.remove('hit');
+                hit = false;
+            }, 1500)
+            if(lives<= 0){
+                endGame();
+                
+            }
+        }
+    }
 }
 
-start.addEventListener('click', startGame);
 
+
+
+
+
+
+
+
+        function endGame() {
+            player.classList.add('dead')
+            holdEnemy = true;
+            restart.style.display = 'flex';
+            restart.addEventListener('click', reload);
+            document.removeEventListener(('keydown', keyDown), 10);
+            document.removeEventListener(('keyup', keyUp), 10);
+
+        }
+
+        function reload() {
+            location.reload();
+
+        }
+
+
+        const start = document.querySelector('#startButton');
+        function startGame() {
+
+            start.style.display = 'none';
+            holdEnemy = false;
+            document.addEventListener('keydown', keyDown);
+            document.addEventListener('keyup', keyUp);
+
+
+
+
+        }
+
+        start.addEventListener('click', startGame);
 
